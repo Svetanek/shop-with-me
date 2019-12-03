8b8e26f65f6606ee30cfc82d61779b9a306bff89
Svetlana Shinkar <svetanek@gmail.com>
2019-12-02 23:45:34 -0500

    sign-in part of sign-in-up-page and a button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInSignUpPage from './pages/sign-in-up-page/sign-in-up-page.component.jsx';
 import './App.css';
 import './homepage.styles.scss';
 
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPage} />
       </Switch>
     </div>
   );
