8b203b5a92b32c6507ff28f97b3b6a56a4f3db47
Svetlana Shinkar <svetanek@gmail.com>
2019-12-18 12:36:22 -0500

    added functionality for checkout strip-button to clear cart and redirect to the main page

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { clearCart } from '../../redux/cart/cart.actions';
 
 const StripeCheckoutButton = ({ price, history, clearCart }) => {
   const priceInCents = price * 100;
   const publishableKey = 'pk_test_6cK6c3cc70YIkRkdqxH2d8Q10052CeFnOC';
   const onToken = token => {
     console.log('TOKEN', token);
+    clearCart();
     alert('Payment Successful');
+    history.push('/');
   };
 
   return (
@@ -24,5 +29,13 @@ const StripeCheckoutButton = ({ price, history, clearCart }) => {
     />
   );
 };
+const mapDispatchToProps = dispatch => ({
+  clearCart: () => dispatch(clearCart()),
+});
 
-export default StripeCheckoutButton;
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(StripeCheckoutButton)
+);
