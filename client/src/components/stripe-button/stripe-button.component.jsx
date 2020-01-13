2371e4161095b609cd6c896e979a3cb67bc57770
Svetlana Shinkar <svetanek@gmail.com>
2020-01-13 14:20:46 -0500

    refactored stripe button onToken to use async await and added clear cart and redirect to the main page

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -9,27 +9,47 @@ const StripeCheckoutButton = ({ price, history, clearCart }) => {
   const priceInCents = price * 100;
   const publishableKey = 'pk_test_6cK6c3cc70YIkRkdqxH2d8Q10052CeFnOC';
 
-  const onToken = token => {
-    axios({
-      url: 'payment',
-      method: 'post',
-      data: {
-        amount: priceInCents,
-        token: token,
-      },
-    })
-      .then(response => {
-        alert('Payment Successful');
-      })
-      .catch(error => {
-        console.log('Payment error', JSON.parse(error));
-        alert(
-          'there was an issue with your payment. Please make sure you use the correct credit card info'
-        );
+  const onToken = async token => {
+    try {
+      const tokenObj = await axios({
+        url: 'payment',
+        method: 'post',
+        data: {
+          amount: priceInCents,
+          token: token,
+        },
       });
-    // clearCart();
-    // alert('Payment Successful');
-    // history.push('/');
+      if (tokenObj) {
+        clearCart();
+        alert('Payment Successful');
+        history.push('/');
+      }
+    } catch (error) {
+      console.log('Payment error', error);
+      alert(
+        'there was an issue with your payment. Please make sure you use the correct credit card info'
+      );
+    }
+    // USING PROMISE
+    // axios({
+    //   url: 'payment',
+    //   method: 'post',
+    //   data: {
+    //     amount: priceInCents,
+    //     token: token,
+    //   },
+    // })
+    //   .then(response => {
+    //     clearCart();
+    //     alert('Payment Successful');
+    //     history.push('/');
+    //   })
+    //   .catch(error => {
+    //     console.log('Payment error', JSON.parse(error));
+    //     alert(
+    //       'there was an issue with your payment. Please make sure you use the correct credit card info'
+    //     );
+    //   });
   };
 
   return (
